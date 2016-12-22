cca1034bacd1e9b4803700b25414d4e914d97b86
LebedenkoNN <totx@narod.ru>
2016-12-22 14:50:01 +0300

    cleanup: nginx config file was added + readme file was improvement

--- README.md
+++ README.md
@@ -1,12 +1,37 @@
-Symfony Standard Edition
+Based on Symfony Standard Edition and some bunles
 ========================
+
+How to configure
+
+```
+echo -e '127.0.0.1\ttest-discount.local' >> /etc/hosts
+ln -sf /var/www/test-discount.local/nginx.conf /etc/nginx/sites-enabled/test-discount.local.conf
+service nginx reload
+chmod -R 777 var/cache
+chmod -R 777 var/logs
+```
 
 How to install
 ```
 composer install
 ```
 
-How to build project
+JS
+---
+
+How to build project for product
 ```
-./node node_modules/webpack/bin/webpack.js
+npm test
+```
+
+How to start dev-server
+```
+npm start
+```
+and than open in browser: http://localhost:8088/webpack-dev-server/
+
+How to restart node.js and dev-server
+```
+killall -9 node
+node dev-server
 ```
--- web/app.php
+++ web/app.php
@@ -1,12 +1,36 @@
 <?php
+// If you don't want to setup permissions the proper way, just uncomment the following PHP line
+// read http://symfony.com/doc/current/setup.html#checking-symfony-application-configuration-and-setup
+// for more information
+//umask(0000);
 
 use Symfony\Component\HttpFoundation\Request;
 
 /** @var \Composer\Autoload\ClassLoader $loader */
-$loader = require __DIR__.'/../app/autoload.php';
-include_once __DIR__.'/../var/bootstrap.php.cache';
+$loader = require __DIR__ . '/../app/autoload.php';
 
-$kernel = new AppKernel('prod', false);
+if (php_sapi_name() === 'cli') {
+    function getOptions()
+    {
+        $options = [];
+        foreach ($_SERVER['argv'] as $arg) {
+            preg_match('/\-\-(\w*)\=?(.+)?/', $arg, $value);
+            if (!empty($value[1])) {
+                $options[$value[1]] = empty($value[2]) ? '' : $value[2];
+            }
+        }
+        return $options;
+    }
+    $options = getOptions();
+    $debug = $options['env'] === 'prod' ? false : true;
+} else {
+    $debug = getenv('SYMFONY_DEBUG') === 'true' || getenv('SYMFONY_ENV') === 'dev';
+}
+if ($debug === false) {
+    include_once __DIR__ . '/../var/bootstrap.php.cache';
+}
+
+$kernel = new AppKernel(($debug === true ? 'dev' : 'prod'), $debug);
 $kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
 
