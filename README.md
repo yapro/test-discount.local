da9514ea338a04b9f10ffca93abad3fad461df46
LebedenkoNN <totx@narod.ru>
2016-12-27 20:20:36 +0300

    cleanup: composer install was improvement + index charset was fixed

--- README.md
+++ README.md
@@ -18,6 +18,11 @@ How to start dev-server
 ```
 and than open in browser: http://localhost:8088/webpack-dev-server/ or http://localhost:8088/
 
+or alternaitve:
+```
+./nodejs/bin/node node_modules/webpack/bin/webpack.js --watch
+```
+
 How to build project for product
 ```
 ./nodejs/bin/node node_modules/webpack/bin/webpack.js -p --display-error-details
--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# install nodejs:
+wget https://nodejs.org/download/release/v6.9.2/node-v6.9.2-linux-x64.tar.gz
+tar -xzf "node-v6.9.2-linux-x64.tar.gz"
+unlink node-v6.9.2-linux-x64.tar.gz
+mv node-v6.9.2-linux-x64 nodejs
+
 # configure web-service:
 echo -e '127.0.0.1\ttest-discount.local' >> /etc/hosts
 ln -sf /var/www/test-discount.local/nginx.conf /etc/nginx/sites-enabled/test-discount.local.conf
@@ -7,10 +13,3 @@ service nginx reload
 chmod -R 777 var/cache
 chmod -R 777 var/logs
 composer install
-
-# install nodejs:
-wget https://nodejs.org/download/release/v6.9.2/node-v6.9.2-linux-x64.tar.gz
-tar -xzf "node-v6.9.2-linux-x64.tar.gz"
-unlink node-v6.9.2-linux-x64.tar.gz
-mv node-v6.9.2-linux-x64 nodejs
-./nodejs/bin/npm i
