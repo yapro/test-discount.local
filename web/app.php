9dcb3b708ce14bd3bed57d1ef8fad9f4befb66e5
LebedenkoNN <totx@narod.ru>
2017-01-16 13:16:55 +0300

    cleanup: app.php for prod env was improved

--- web/app.php
+++ web/app.php
@@ -13,8 +13,6 @@ if ($debug === false) {
     include_once __DIR__ . '/../var/bootstrap.php.cache';
 }
 $kernel = new AppKernel(($debug === true ? 'dev' : 'prod'), $debug);
-$kernel->loadClassCache();
-
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
 //Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
